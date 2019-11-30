19ad8551bedebeeb13ed9cb0513b6431c04b2498
Hash&Salt <hello@hashandsalt.com>
2019-11-30 22:26:26 +0000

    Shiny orders table

--- src/components/dashboard/popular.vue
+++ src/components/dashboard/popular.vue
@@ -6,31 +6,40 @@
 </template>
 <script>
 export default {
+  name: 'popularItem',
   data() {
     return {
-      popularItem: null
+      popularItem: undefined,
+      errors: [],
     }
   },
   computed: {
      popularCount () {
-       return this.popularItem ? this.popularItem : 'Loading...'
+       return (this.popularItem) ? this.popularItem : 'Loading...'
      }
   },
   // Abandoned Carts
   async created() {
-    let response = await this.$api.get('snipcart/orders');
+    try {
 
-    let flattened = response.items.reduce((a, b) => a.concat(b.items), []);
+      let response = await this.$api.get('snipcart/orders', {pretty: true})
 
-    let flattenedIDs = flattened.map(({ name }) => name).reduce((names, name) => {
-        const count = names[name] || 0;
-        names[name] = count + 1;
-        return names;
-      }, {});
 
-    let popularID = Object.keys(flattenedIDs).reduce((a, b) => flattenedIDs[a] > flattenedIDs[b] ? a : b);
+          let flattened = response.items.reduce((a, b) => a.concat(b.items), []);
 
-    this.popularItem = popularID;
+          let flattenedIDs = flattened.map(({ name }) => name).reduce((names, name) => {
+              const count = names[name] || 0;
+              names[name] = count + 1;
+              return names;
+            }, {});
+
+          let popularID = Object.keys(flattenedIDs).reduce((a, b) => flattenedIDs[a] > flattenedIDs[b] ? a : b);
+
+          this.popularItem = popularID;
+
+    } catch (e) {
+       this.errors.push(e)
+    }
   }
 }
 </script>
